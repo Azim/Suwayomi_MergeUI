0e2e8b3061acc059ae05ddeb9833b662933f0a24
Alexey Borisenko <frozenpriest@mail.ru>
2025-09-25 18:47:08 +0300

    moved schema to root

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,14 @@
 package ru.frozenpriest
 
+import com.expediagroup.graphql.client.ktor.GraphQLKtorClient
+import com.expediagroup.graphql.client.serialization.GraphQLClientKotlinxSerializer
+import java.net.URL
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-
+    val client = GraphQLKtorClient(
+        url = URL("http://localhost:8080/graphql"),
+        serializer = GraphQLClientKotlinxSerializer(),
+    )
 }

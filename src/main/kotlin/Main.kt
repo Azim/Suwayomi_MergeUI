925edc93e0d315f54d54637350d09d79472071e8
Alexey Borisenko <frozenpriest@mail.ru>
2025-09-25 21:27:25 +0300

    testing request

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,13 +2,20 @@ package ru.frozenpriest
 
 import com.expediagroup.graphql.client.ktor.GraphQLKtorClient
 import com.expediagroup.graphql.client.serialization.GraphQLClientKotlinxSerializer
+import kotlinx.coroutines.runBlocking
+import ru.frozenpriest.generated.NewMangaChapters
 import java.net.URL
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     val client = GraphQLKtorClient(
-        url = URL("http://localhost:8080/graphql"),
+        url = URL("http://192.168.50.182:4567/api/graphql"),
         serializer = GraphQLClientKotlinxSerializer(),
     )
+
+    runBlocking {
+        val result = client.execute(NewMangaChapters(NewMangaChapters.Variables(today = "0")))
+        println(result.data?.chapters?.nodes?.joinToString(separator = "\n"))
+    }
 }
